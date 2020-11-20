7eab12ca6cb81c18aefb79ee0282d94d5b4f6c93
vikkyfama <64605523+vikkyfama@users.noreply.github.com>
2020-11-20 22:00:26 +0100

    Update train.py
    Updated train.py

--- train.py
+++ train.py
@@ -9,20 +9,22 @@ from sklearn.preprocessing import OneHotEncoder
 import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
+from sklearn import preprocessing
 
 # TODO: Create TabularDataset using TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+dat = "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+ds = TabularDatasetFactory.from_delimited_files(dat)
 
-x, y = clean_data(ds)
+# x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
 
-### YOUR CODE HERE ###a
+# ## YOUR CODE HERE ###a
 
-run = Run.get_context()
+# run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -49,24 +51,48 @@ def clean_data(data):
     x_df["poutcome"] = x_df.poutcome.apply(lambda s: 1 if s == "success" else 0)
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
-    
+    return x_df, y_df
+
+x, y = clean_data(ds)
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.30)
+
+# Normalizing the input data
+normalizer = preprocessing.Normalizer().fit(x_train)
+normalizer.transform(x_train)
+normalizer = preprocessing.Normalizer().fit(x_test)
+normalizer.transform(x_test)    
+
 
 def main():
     # Add arguments to script
     parser = argparse.ArgumentParser()
 
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
-    parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
+    parser.add_argument('--max_iter', type=int, default=1000, help="Maximum number of iterations to converge")
 
-    args = parser.parse_args()
+    args = parser.parse_args(args = [])
 
+    run = Run.get_context()
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
 
     model = LogisticRegression(C=args.C, max_iter=args.max_iter).fit(x_train, y_train)
 
+    y_pred = model.predict(x_test)
+    mean_squared_error(y_pred, y_test)
     accuracy = model.score(x_test, y_test)
+
+    run = Run.get_context()
     run.log("Accuracy", np.float(accuracy))
+
+    #Save the trained model
+    os.makedirs('outputs', exist_ok=True)
+    print('model saved')
+    joblib.dump(value=model, filename='outputs/model.pkl')
+    print('dumped')
+
+    run.complete()
+
 
 if __name__ == '__main__':
     main()
